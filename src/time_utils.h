03216a35243e7f0d610c579ca04806fd03774d5f
Vinicius Fonseca <vfonseca1618@gmail.com>
2026-05-13 18:58:12 -0300

    submission

--- src/time_utils.h
+++
@@ -1,9 +0,0 @@
-#ifndef RINHA_TIME_UTILS_H
-#define RINHA_TIME_UTILS_H
-
-#include <stdint.h>
-
-int rinha_weekday_monday0(int year, int month, int day);
-int64_t rinha_epoch_minutes_utc(int year, int month, int day, int hour, int minute, int second);
-
-#endif
